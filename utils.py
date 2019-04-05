fa59a5f40b9034d4ccadd939389e813b7404f069
Hiromichi Kamata <1216kamata@gmail.com>
2019-04-05 03:07:36 -0400

    add gitignore

---
+++ utils.py
@@ -0,0 +1,72 @@
+from __future__ import print_function, division
+import matplotlib.pyplot as plt
+import math
+from scipy.ndimage.filters import convolve
+from sklearn.metrics import auc
+import numpy as np
+import cv2
+import os, sys
+int_ = lambda x: int(round(x))
+def IoU( r1, r2 ):
+    x11, y11, w1, h1 = r1
+    x21, y21, w2, h2 = r2
+    x12 = x11 + w1; y12 = y11 + h1
+    x22 = x21 + w2; y22 = y21 + h2
+    x_overlap = max(0, min(x12,x22) - max(x11,x21) )
+    y_overlap = max(0, min(y12,y22) - max(y11,y21) )
+    I = 1. * x_overlap * y_overlap
+    U = (y12-y11)*(x12-x11) + (y22-y21)*(x22-x21) - I
+    J = I/U
+    return J
+def evaluate_iou( rect_gt, rect_pred ):
+    # score of iou
+    score = [ IoU(i, j) for i, j in zip(rect_gt, rect_pred) ]
+    return score
+
+def compute_score( x, w, h ):
+    # score of response strength
+    k = np.ones( (h, w) )
+    score = convolve( x, k, mode='wrap' )
+    score[:, :w//2] = 0
+    score[:, math.ceil(-w/2):] = 0
+    score[:h//2, :] = 0
+    score[math.ceil(-h/2):, :] = 0
+    return score
+def locate_bbox( a, w, h ):
+    row = np.argmax( np.max(a, axis=1) )
+    col = np.argmax( np.max(a, axis=0) )
+    x = col - 1. * w / 2
+    y = row - 1. * h / 2
+    return x, y, w, h
+def score2curve( score, thres_delta = 0.01 ):
+    thres = np.linspace( 0, 1, int(1./thres_delta)+1 )
+    success_num = []
+    for th in thres:
+        success_num.append( np.sum(score >= (th+1e-6)) )
+    success_rate = np.array(success_num) / len(score)
+    return thres, success_rate
+
+    return bboxes
+def all_sample_iou( score_list, gt_list):
+    num_samples = len(score_list)
+    iou_list = []
+    for idx in range(num_samples):
+        score, image_gt = score_list[idx], gt_list[idx]
+        w, h = image_gt[2:]
+        pred_rect = locate_bbox( score, w, h )
+        iou = IoU( image_gt, pred_rect )
+        iou_list.append( iou )
+    return iou_list
+
+
+def plot_success_curve( iou_score, title='' ):
+    thres, success_rate = score2curve( iou_score, thres_delta = 0.05 )
+    auc_ = np.mean( success_rate[:-1] ) # this is same auc protocol as used in previous template matching papers #auc_ = auc( thres, success_rate ) # this is the actual auc
+    plt.figure()
+    plt.grid(True)
+    plt.xticks(np.linspace(0,1,11))
+    plt.yticks(np.linspace(0,1,11))
+    plt.ylim(0, 1)
+    plt.title(title + 'auc={}'.format(auc_))
+    plt.plot( thres, success_rate )
+    plt.show()
